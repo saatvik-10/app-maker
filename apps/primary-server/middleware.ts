475557a868da4ef6752f94bee90efb47cc674d25
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-29 00:36:41 +0530

    fixes

--- apps/primary-server/middleware.ts
+++ apps/primary-server/middleware.ts
@@ -2,7 +2,8 @@ import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) {
         res.status(401).json({
@@ -11,6 +12,7 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
         return;
     }
 
+    console.log(process.env.JWT_PUBLIC_KEY)
     const decoded = jwt.verify(token, process.env.JWT_PUBLIC_KEY!, {
         algorithms: ["RS256"]
     })
@@ -23,7 +25,7 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
     }
 
     //getting userid from clerk
-    const userId = (decoded as any).payload.sub;
+    const userId = (decoded as any).sub;
 
     if (!userId) {
         res.status(401).json({

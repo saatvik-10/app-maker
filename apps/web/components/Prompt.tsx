da47d1e7eafea112db889bae0ba906e1da96b34f
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-29 00:36:59 +0530

    post endpoint

--- apps/web/components/Prompt.tsx
+++ apps/web/components/Prompt.tsx
@@ -1,16 +1,46 @@
+'use client';
+
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from './ui/button';
 import { Send } from 'lucide-react';
+import axios from 'axios';
+import { useState } from 'react';
+import { useAuth } from '@clerk/nextjs';
+import { BACKEND_URL } from '@/lib/config';
 
 const Prompt = () => {
+  const [prompt, setPrompt] = useState<string>('');
+  const { getToken } = useAuth();
+
+  const handlePrompt = async () => {
+    const token = await getToken();
+    const res = axios.post(
+      `${BACKEND_URL}/projects`,
+      {
+        prompt: prompt,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(res);
+  };
+
   return (
     <div>
       <Textarea
         className='h-24 text-white'
         placeholder='Create a Billion Dollar idea...'
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
       />
       <div className='flex justify-end pt-2'>
-        <Button className='bg-white hover:bg-red-200 cursor-pointer'>
+        <Button
+          className='bg-white hover:bg-red-200 cursor-pointer'
+          onClick={handlePrompt}
+        >
           <Send className='text-red-800' />
         </Button>
       </div>

7fb6816cd663e7b24bc4392aec400e5ad049cd3a
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-28 23:15:48 +0530

    hero section

---
+++ apps/web/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className='flex flex-col gap-y-2 items-center'>
+      <span className='font-bold text-3xl text-white'>
+        Want to build something? Try me :)
+      </span>
+      <p className='text-gray-400 text-lg font-medium'>
+        Spark the idea, smash generate, and boom — your app’s on fire.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;

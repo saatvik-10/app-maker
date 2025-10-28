12c454ba43e56f2e9fc94b3f9546758fbf11edc8
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-28 23:15:52 +0530

    prompt area

---
+++ apps/web/components/Prompt.tsx
@@ -0,0 +1,14 @@
+import { Textarea } from '@/components/ui/textarea';
+
+const Prompt = () => {
+  return (
+    <div>
+      <Textarea
+        className='h-24 text-white'
+        placeholder='Create a Billion Dollar idea...'
+      />
+    </div>
+  );
+};
+
+export default Prompt;

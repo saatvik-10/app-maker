078994e1ee107565a882b10b6c074ae8fdc96a33
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-28 23:22:39 +0530

    send btn

--- apps/web/components/Prompt.tsx
+++ apps/web/components/Prompt.tsx
@@ -1,4 +1,6 @@
 import { Textarea } from '@/components/ui/textarea';
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
 
 const Prompt = () => {
   return (
@@ -7,6 +9,11 @@ const Prompt = () => {
         className='h-24 text-white'
         placeholder='Create a Billion Dollar idea...'
       />
+      <div className='flex justify-end pt-2'>
+        <Button className='bg-white hover:bg-red-200 cursor-pointer'>
+          <Send className='text-red-800' />
+        </Button>
+      </div>
     </div>
   );
 };

bd3ad21e5156be43ae5ca26acdfd6d191e9e06cb
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-31 01:25:02 +0530

    updated prompt endpoint

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -1,22 +1,31 @@
 import express from 'express';
 import cors from 'cors'
-import {prismaClient} from "db/client"
+import { prismaClient } from "db/client"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.post("/prompt", async(req, res) => {
-   const {prompt, projectId} = req.body 
+app.post("/prompt", async (req, res) => {
+    const { prompt, projectId } = req.body
 
-   await prismaClient.prompt.create({
-    data: {
-        content: prompt,
-        projectId,
-        type: "USER"
-    }
-   })
+    await prismaClient.prompt.create({
+        data: {
+            content: prompt,
+            projectId,
+            type: "USER"
+        }
+    })
+
+    const allPrompts = await prismaClient.prompt.findMany({
+        where: {
+            projectId
+        },
+        orderBy: {
+            createdAt: "asc"
+        }
+    })
 })
 
 app.listen(8081)
